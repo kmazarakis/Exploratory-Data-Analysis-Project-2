ea3871b76a134dafa044054291d41f60d73c47d0
kmazarakis <kleanthis.mazarakis@gmail.com>
2017-09-19 16:33:53 +0100

    Update plot5.R

--- plot5.R
+++ plot5.R
@@ -1,1 +1,18 @@
+## This first line will likely take a few seconds. Be patient!
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
 
+##Find Vehicle Related sources from SCC
+SCCvehicle <- grepl("vehicle", SCC$EI.Sector, ignore.case = T)
+SCCvehiclefinal <- SCC[SCCvehicle, ]
+
+##Find NEI Vehicle related from SCCvehiclefinal
+NEIVehicleBaltimore <- NEI[NEI$SCC %in% SCCvehiclefinal$SCC & NEI$fips == "24510", ]
+
+##Aggregate emission sums per year
+data5 <- with(NEIVehicleBaltimore, tapply(Emissions, year, sum))
+
+##Plot the graph
+barplot(data5, names.arg = names(data5), main = "Total Vehicle Related Emissions per Year in Baltimore", xlab = "Years", ylab = "Emissions")
+dev.copy(png, "plot4.png")
+dev.off()
